dd70462a83c0740ee2b3ab59e787c19f891ce350
shreyashish <shreyashishsengupta@gmail.com>
2017-08-06 16:45:22 +0530

    fin

--- src/App.js
+++ src/App.js
@@ -2,26 +2,52 @@ import React, {Component} from 'react';
 import './App.css';
 import TasksList from './TasksList'
 
-const tasks = [
-    {
-        "id": 1,
-        "content": "clean house"
-    },
-    {
-        "id": 2,
-        "content": "do puja everyday"
-    },
-    {
-        "id": 3,
-        "content": "watch Rick n Morty"
-    },
-];
+// const tasks = [
+//     {
+//         "id": 1,
+//         "content": "clean house"
+//     },
+//     {
+//         "id": 2,
+//         "content": "do puja everyday"
+//     },
+//     {
+//         "id": 3,
+//         "content": "watch Rick n Morty"
+//     },
+// ];
 
 class App extends Component {
+    state = {
+        tasks: [
+            {
+                "id": 1,
+                "content": "clean house"
+            },
+            {
+                "id": 2,
+                "content": "do puja everyday"
+            },
+            {
+                "id": 3,
+                "content": "watch Rick n Morty"
+            },
+        ]
+    };
+
+    removeTask = (task) => {
+        this.setState((prevState) => ({
+            tasks: prevState.tasks.filter((oldTask) => oldTask.id !== task.id)
+        }))
+    };
+
     render() {
         return (
             <div>
-                <TasksList tasks={tasks}/>
+                <TasksList
+                    tasks={this.state.tasks}
+                    onCompletion={this.removeTask}
+                />
             </div>
         );
     }
--- src/TasksList.js
+++ src/TasksList.js
@@ -1,14 +1,52 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
+import escapeRegExp from 'escape-string-regexp'
 
 class TasksList extends Component {
+    state = {
+        query: ''
+    };
+
+    handleChange = (query) => {
+        this.setState({
+            query: query
+        });
+    };
+
     render() {
         //console.log(this.props);
-        return(
-            <ul>
-                {this.props.tasks.map((task) => (
-                    <li key={task.id}>{task.content}</li>
-                ))}
-            </ul>
+        let filteredTasks;
+        if(this.state.query) {
+            const match = new RegExp(escapeRegExp(this.state.query, 'i'));
+            // console.log(match.test('clean'));
+            filteredTasks = this.props.tasks.filter((task) => match.test(task.content));
+        } else {
+            filteredTasks = this.props.tasks;
+        }
+
+        return (
+            <div>
+                <input
+                    type="text"
+                    placeholder="search"
+                    value={this.state.query}
+                    onChange={(event) => this.handleChange(event.target.value)}
+                />
+                {/*{this.state.query.length < 6 && (*/}
+                    {/*<div>*/}
+                        {/*Please write more than {6-this.state.query.length} characters*/}
+                    {/*</div>*/}
+                {/*)}*/}
+                <ul>
+                    {filteredTasks.map((task) => (
+                        <li key={task.id}>
+                            {task.content}
+                            <button onClick={() => this.props.onCompletion(task)}>
+                                Complete
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         )
     }
 }
